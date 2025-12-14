c1b248321ad4f24c894156c52933a1a1812348ed
Joan Maina <mainajoan555@gmail.com>
2025-12-14 12:45:50 +0300

    product app settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'mainapp'
+    'mainapp',
+    'products',
 ]
 
 AUTH_USER_MODEL = 'mainapp.User'
