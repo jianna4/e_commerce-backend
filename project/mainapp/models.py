17bcae1f927f283eb8b63ff79e779b9b65fad12f
Joan Maina <mainajoan555@gmail.com>
2025-12-14 11:44:00 +0300

    model

--- project/mainapp/models.py
+++ project/mainapp/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+
 
 # Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from .manager import UserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)   # staff/admin
+    is_superuser = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
