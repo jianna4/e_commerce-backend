f743ee46dc0b6bc3d473b4411548f80f03336f4f
Joan Maina <mainajoan555@gmail.com>
2025-12-15 07:41:21 +0300

    model changes

--- project/products/models.py
+++ project/products/models.py
@@ -24,6 +24,15 @@ class Category(models.Model):
    created_at = models.DateTimeField(auto_now_add=True)
    def __str__(self):
         return self.name
+   
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="subcategories")
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
+
 
 
 # --------------------
