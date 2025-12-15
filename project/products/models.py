a3595c8bfbc46dec30bbec4c281e388314dabd95
Joan Maina <mainajoan555@gmail.com>
2025-12-15 06:47:34 +0300

    class model

--- project/products/models.py
+++ project/products/models.py
@@ -7,14 +7,22 @@ from django.conf import settings
 # --------------------
 # Category
 # --------------------
+# products/models.py
+from django.db import models
+
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="categories/", blank=True)
+
+    is_active = models.BooleanField(default=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
 
 # --------------------
 # Product
