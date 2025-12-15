2e81b3b31ec3fb5d4cf64066146b3d5a4caefa1c
Joan Maina <mainajoan555@gmail.com>
2025-12-15 15:23:10 +0300

    changes

--- project/products/urls.py
+++ project/products/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from .views import (
     CategoryListView, CategoryDetailView,
+    SubCategoryListView, SubCategoryDetailView,
     ProductListView, ProductDetailView,
-    OrderListView, OrderDetailView
+    OrderListView, OrderDetailView,
 )
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view()),
     path('categories/<int:pk>/', CategoryDetailView.as_view()),
+
+    path('subcategories/', SubCategoryListView.as_view()),
+    path('subcategories/<int:pk>/', SubCategoryDetailView.as_view()),
 
     path('products/', ProductListView.as_view()),
     path('products/<int:pk>/', ProductDetailView.as_view()),
