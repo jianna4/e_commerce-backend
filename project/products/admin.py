068b81cfea35bf7158f48b1a2bbaac334d4a958b
Joan Maina <mainajoan555@gmail.com>
2025-12-16 17:56:52 +0300

    product changes admin

--- project/products/admin.py
+++ project/products/admin.py
@@ -2,17 +2,27 @@
 
 # Register your models here.
 from django.contrib import admin
-from .models import Category, Product, Order, OrderItem ,SubCategory
+from .models import Category, Product, Order, OrderItem ,SubCategory ,ProductColor ,ProductImage
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'created_at']
     prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
     
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'category']
     search_fields = ['name', 'category__name']
+    list_filter = ('category',)
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+class ProductColorInline(admin.TabularInline):
+    model = ProductColor
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -20,6 +30,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['available', 'subcategory']
     search_fields = ['name', 'description']
     prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline, ProductColorInline]
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
