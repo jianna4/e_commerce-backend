a33ca091959e71844694402c5f94e25dc37b4372
Joan Maina <mainajoan555@gmail.com>
2025-12-16 22:52:42 +0300

    login credentials

--- project/mainapp/views.py
+++ project/mainapp/views.py
@@ -6,3 +6,19 @@ from .serializers import RegisterSerializer
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def me(request):
+    user = request.user
+    return Response({
+        "id": user.id,
+        "username": user.username,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "email": user.email,
+    })
