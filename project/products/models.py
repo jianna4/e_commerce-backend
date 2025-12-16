a838d69aabbd1921ee16a15ddca77138248ba680
Joan Maina <mainajoan555@gmail.com>
2025-12-16 18:13:03 +0300

    product price

--- project/products/models.py
+++ project/products/models.py
@@ -119,10 +119,16 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    # New optional fields
+    size = models.CharField(max_length=50, blank=True, null=True, help_text="Optional size")
+    color = models.CharField(max_length=50, blank=True, null=True, help_text="Optional color")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     total = models.DecimalField(max_digits=10, decimal_places=2, editable=False)  # new field
 
     def save(self, *args, **kwargs):
+         # Set price from product if not already set
+        if not self.price:
+            self.price = self.product.price
         # Automatically calculate total before saving
         self.total = self.quantity * self.price
         super().save(*args, **kwargs)
