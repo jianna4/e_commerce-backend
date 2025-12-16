9a8f024b0b4b202772fe37305a18602b0b816550
Joan Maina <mainajoan555@gmail.com>
2025-12-16 23:53:57 +0300

    serializer changes

--- project/mainapp/urls.py
+++ project/mainapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import RegisterView ,me
 from rest_framework_simplejwt.views import TokenObtainPairView
+from . import views
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', TokenObtainPairView.as_view()),
-    path('me/', me.as_view())
+      path('me/', views.me, name='me'),
+   
 
 ]
