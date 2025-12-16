4a630cc313fa1487034359033e370cbbf9fa9994
Joan Maina <mainajoan555@gmail.com>
2025-12-16 18:26:34 +0300

    model changes

--- project/products/models.py
+++ project/products/models.py
@@ -123,7 +123,7 @@ class OrderItem(models.Model):
     size = models.CharField(max_length=50, blank=True, null=True, help_text="Optional size")
     color = models.CharField(max_length=50, blank=True, null=True, help_text="Optional color")
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    total = models.DecimalField(max_digits=10, decimal_places=2, editable=False)  # new field
+    total = models.DecimalField(max_digits=10, decimal_places=2, editable=False,blank=True, null=True)  # new field
 
     def save(self, *args, **kwargs):
          # Set price from product if not already set
