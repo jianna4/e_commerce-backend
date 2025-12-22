4308af2ba48018fc6e35a513a44cea0590c48338
Joan Maina <mainajoan555@gmail.com>
2025-12-22 05:32:53 +0300

    initials veiw

--- project/mainapp/views.py
+++ project/mainapp/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from rest_framework import generics
 from .serializers import RegisterSerializer
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+@api_view(["POST"])
+def register_view(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response({
+            "id": user.id,
+            "email": user.email,
+            "full_name": user.full_name,
+            "initials": serializer.data.get("initials")
+        }, status=201)
+    return Response(serializer.errors, status=400)
+
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
