11dd3cd1b6a36e0f2ce4862a43dc5cfb1dbdf61d
Joan Maina <mainajoan555@gmail.com>
2025-12-23 06:39:25 +0300

    serializer register remore initils

--- project/mainapp/serializers.py
+++ project/mainapp/serializers.py
@@ -3,17 +3,13 @@ from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    initials = serializers.SerializerMethodField()
+   
 
     class Meta:
         model = User
         fields = ['email', 'full_name', 'password']
     
-    def get_initials(self,obj):
-        if obj.full_name:
-          names= obj.full_name.strip().split()
-          return ''.join([name[0].upper() for name in names[:2]])
-        return obj.email[0].upper()
+    
 
 
     def create(self, validated_data):
